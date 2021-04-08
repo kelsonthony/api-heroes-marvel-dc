fadbb058b9098753e335f9c53dba7ff96f56c5ad
kelsonthony <k.thony@gmail.com>
2021-04-08 17:41:18 -0300

    04-version done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const ContextStrategy = require('./db/strategies/base/contextStrategy')
-const MongoDB = require('./db/strategies/mongodb')
-const Postgres = require('./db/strategies/postgres')
+const MongoDB = require('./db/strategies/mongo/mongo')
+const Postgres = require('./db/strategies/postgres/postgres')
 
 const contextMongo = new ContextStrategy(new MongoDB())
 contextMongo.create()
